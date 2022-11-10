fde1597e35c4b37c81b40d20232906dbd83ff318
Garfield Greg V. Lim III <garfield.lim@usjr.edu.ph>
2022-11-10 13:23:36 +0800

    Initial commit
    Initial commit from local repo

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
+
+function App() {
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(true);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({
+    show: false,
+    message: "",
+    type: "",
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!name){
+      // display alert
+    }
+    else if(name && isEditing){
+      // deal with edit
+    }
+    else {
+      // show alert
+      const newItem = {id: new Date().getTime().toString(),
+      title:name};
+      setList([...list,newItemn]);
+      setName('')
+    }
+    }
+  
+
+  return (
+    <section className="section-center">
+      meowk
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>Grocery Bud</h3>
+        <div className="form-control">
+          <input
+            type='text'
+            className='grocery'
+            placeholder='e.g. eggslyn'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "edit" : "submit"}
+          </button>
+        </div>
+      </form>
+      <div className="grocery-container">
+        <List />
+        <button className="clear-btn">clear items</button>
+      </div>
+    </section>
+  );
+  };
+export default App;
